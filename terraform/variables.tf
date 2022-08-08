005d755259a790d47484966edc6e7166bce28214
Kevin Printz <kevinprintz@microsoft.com>
2022-08-08 12:14:56 -0400

    create the template

---
+++ terraform/variables.tf
@@ -0,0 +1,31 @@
+variable "location" {
+  type = string
+}
+
+variable "rg_name" {
+  type = string
+}
+
+variable "gallery_name" {
+  type = string
+}
+
+variable "image_name" {
+  type = string
+}
+
+variable "image_type" {
+  type = string
+}
+
+variable "image_publisher" {
+  type = string
+}
+
+variable "image_offer" {
+  type = string
+}
+
+variable "image_sku" {
+  type = string
+}
