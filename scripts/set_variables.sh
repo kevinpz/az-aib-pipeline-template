efe6a0cb5744f5a88e08be59adb2d9d1f02b795f
Kevin Printz <kevinprintz@microsoft.com>
2022-08-12 15:25:57 -0400

    fix

--- scripts/set_variables.sh
+++ scripts/set_variables.sh
@@ -1,11 +1,11 @@
-sed -i -e "s/<subscription_id>/${{ variables.subscription_id }}/g" ../template/image_template.json
-sed -i -e "s/<rg_name>/${{ variables.rg_name }}/g" ../template/image_template.json
-sed -i -e "s/<image_name>/${{ parameters.image_name }}/g" ../template/image_template.json
-sed -i -e "s/<gallery_name>/${{ variables.gallery_name }}/g" ../template/image_template.json
-sed -i -e "s/<region_1>/${{ variables.location }}/g" ../template/image_template.json
-sed -i -e "s/<id_name>/${{ variables.id_name }}/g" ../template/image_template.json
-sed -i -e "s/<src_image_publisher>/${{ parameters.src_image_publisher }}/g" ../template/image_template.json
-sed -i -e "s/<src_image_offer>/${{ parameters.src_image_offer }}/g" ../template/image_template.json
-sed -i -e "s/<src_image_sku>/${{ parameters.src_image_sku }}/g" ../template/image_template.json
-sed -i -e "s/<image_build_sku>/${{ parameters.image_build_sku }}/g" ../template/image_template.json
-sed -i -e "s/<image_build_size>/${{ parameters.image_build_size }}/g" ../template/image_template.json
+sed -i -e "s/<subscription_id>/${variables.subscription_id}/g" ../template/image_template.json
+sed -i -e "s/<rg_name>/${variables.rg_name}/g" ../template/image_template.json
+sed -i -e "s/<image_name>/${parameters.image_name}/g" ../template/image_template.json
+sed -i -e "s/<gallery_name>/${variables.gallery_name}/g" ../template/image_template.json
+sed -i -e "s/<region_1>/${variables.location}/g" ../template/image_template.json
+sed -i -e "s/<id_name>/${variables.id_name}/g" ../template/image_template.json
+sed -i -e "s/<src_image_publisher>/${parameters.src_image_publisher}/g" ../template/image_template.json
+sed -i -e "s/<src_image_offer>/${parameters.src_image_offer}/g" ../template/image_template.json
+sed -i -e "s/<src_image_sku>/${parameters.src_image_sku}/g" ../template/image_template.json
+sed -i -e "s/<image_build_sku>/${parameters.image_build_sku}/g" ../template/image_template.json
+sed -i -e "s/<image_build_size>/${parameters.image_build_size}/g" ../template/image_template.json
